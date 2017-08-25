2a2b11f78ee64cd54cdfb8d105606215c42dcaa5
FXelix <felix.scheling@web.de>
2017-08-25 14:27:11 +0200

    Update 0.0.1 - Added imports - Added standards for accessing Twitter API

--- bot.py
+++ bot.py
@@ -1,1 +1,8 @@
 
+import tweepy
+from secrets import *
+
+# standard for accessing Twitter API
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
