8b0c08962d18536b87a948c96f7ec7daabd8b4e1
FXelix <felix.scheling@web.de>
2017-08-30 10:34:50 +0200

    Update 0.1.1 - Added api number reference

--- NEO_flyby.py
+++ NEO_flyby.py
@@ -4,10 +4,23 @@ import datetime
 import requests
 import json
 
+
 def get_NEO_flyby():
 
     neo_data = []
 
+    des = 0
+    orbit_id = 1
+    jd = 2
+    cd = 3
+    dist = 4
+    dist_min = 5
+    dist_max = 6
+    v_rel = 7
+    v_inf = 8
+    t_signma_F = 9
+    body = 10
+    h = 11
 
     unix = time.time()
     datestamp = datetime.datetime.fromtimestamp(unix).strftime("%Y-%b-%d")
@@ -15,11 +28,11 @@ def get_NEO_flyby():
     json_data_url = requests.get("https://ssd-api.jpl.nasa.gov/cad.api?body=Earth&dist-max=20LD")
     json_data = json.loads(json_data_url.text)
     for i in range(len(json_data["data"])):
-        neo_date = json_data["data"][i][3][:11]
-        neo_time = json_data["data"][i][3][11:]
+        neo_date = json_data["data"][i][cd][:11]
+        neo_time = json_data["data"][i][cd][11:]
 
         if neo_date == datestamp:
-            neo_data.append((json_data["data"][i][0],))
+            neo_data.append((json_data["data"][i][des],))
 
 
 
@@ -32,7 +45,6 @@ def get_NEO_flyby():
 get_NEO_flyby()
 
 
-# TODO: Add api indicator of numbers
 # TODO: Iterate over data and return tuple
 
 
