2ba83483fecc21e591abfdcbfb08812092e149fb
FXelix <felix.scheling@web.de>
2017-08-30 11:03:42 +0200

    Update 0.3.0 - imported log10 from math - fixed indention - commented number reference out - addef missing self parameter return list with tuples of data

--- NEO_flyby.py
+++ NEO_flyby.py
@@ -3,10 +3,12 @@ import time
 import datetime
 import requests
 import json
+from math import log10
 
 
 class NEO:
-
+    """
+    For accesing the data, corresponding numbers:
     des = 0
     orbit_id = 1
     jd = 2
@@ -19,39 +21,25 @@ class NEO:
     t_signma_F = 9
     body = 10
     h = 11
-
+    """
     json_data_url = requests.get("https://ssd-api.jpl.nasa.gov/cad.api?body=Earth&dist-max=20LD")
     json_data = json.loads(json_data_url.text)
 
     neo_data = []
 
-    def estimated_diameter(self):
-        # todo
-
     def flyby_data(self):
 
-    unix = time.time()
-    datestamp = datetime.datetime.fromtimestamp(unix).strftime("%Y-%b-%d")
+        unix = time.time()
+        datestamp = datetime.datetime.fromtimestamp(unix).strftime("%Y-%b-%d")
 
-    for i in range(len(json_data["data"])):
-        neo_date = json_data["data"][i][cd][:11]
-        neo_time = json_data["data"][i][cd][11:]
-        neo_des = json_data["data"][i][des]
+        for i in range(len(self.json_data["data"])):
+            neo_date = self.json_data["data"][i][3][:11]
+            neo_time = self.json_data["data"][i][3][11:]
+            neo_des = self.json_data["data"][i][0]
+            diameter_min = 10 ** 0.5*(6.259 - log10(0.05) - 0.4 * self.json_data["data"][i][11])
+            diameter_max = 10 ** 0.5*(6.259 - log10(0.25) - 0.4 * self.json_data["data"][i][11])
 
-        if neo_date == datestamp:
-            neo_data.append((neo_des, neo_time,))
-
-
-
-
-
-
-
-        
-        # sorte lieber per magnitude und nimm nur das größte objekt, sonst ist der tweet zu lang
-
-
-
-# TODO: Iterate over data and return tuple
-
-
+            if neo_date == datestamp:
+                self.neo_data.append((neo_des, neo_time, diameter_min, diameter_max))
+                
+        return self.neo_data
